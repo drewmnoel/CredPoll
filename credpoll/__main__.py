b511773798d635bb7649a6d13e3f6c48de00635e
Drew Noel <drew.noel@aon.com>
2024-09-10 11:25:14 -0400

    Lint

--- credpoll/__main__.py
+++ credpoll/__main__.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 from datetime import datetime
+
 from credpoll.util import get_cred_files, get_hashes
 
 __version__ = "0.1"
@@ -13,8 +14,9 @@ def list_accounts(logs_dir: str) -> list:
     new_accounts = []
     for hash in hashes:
         identity = f"{hash.split(':')[2]}\\{hash.split(':')[0]}"
-        if (identity.lower() not in map(str.lower, new_accounts) and
-                identity.lower() not in map(str.lower, CACHE)):
+        if identity.lower() not in map(
+            str.lower, new_accounts
+        ) and identity.lower() not in map(str.lower, CACHE):
             new_accounts.append(identity)
 
     return new_accounts
@@ -26,23 +28,25 @@ def extract_creds(logs_dir: str) -> str:
     option_idx = 0
     for account in accounts:
         option_idx += 1
-        print(f'{option_idx}) {account}')
+        print(f"{option_idx}) {account}")
     print()
     try:
         extract_account = int(input("[!] Pick an account to extract credentials for: "))
     except ValueError:
         sys.exit("[-] Invalid choice!")
 
-    if (extract_account-1) not in range(0, len(accounts)):
-        print('[-] Invalid choice!')
+    if (extract_account - 1) not in range(0, len(accounts)):
+        print("[-] Invalid choice!")
         sys.exit(1)
     else:
-        extract_account = accounts[extract_account-1]
+        extract_account = accounts[extract_account - 1]
 
     hashes = get_hashes(get_cred_files(logs_dir))
     for hash in hashes:
-        if (hash.split(":")[0] == extract_account.split("\\")[1] and
-                hash.split(":")[2] == extract_account.split("\\")[0]):
+        if (
+            hash.split(":")[0] == extract_account.split("\\")[1]
+            and hash.split(":")[2] == extract_account.split("\\")[0]
+        ):
             return hash
 
     return "ERROR: Hash not found"
@@ -53,8 +57,9 @@ def generate_hashlist(logs_dir: str):
     hashlist = []
     for hash in hashes:
         identity = f"{hash.split(':')[2]}\\{hash.split(':')[0]}"
-        if (identity.lower() not in map(str.lower, CACHE)
-                and identity.lower() not in map(str.lower, hashlist)):
+        if identity.lower() not in map(
+            str.lower, CACHE
+        ) and identity.lower() not in map(str.lower, hashlist):
             hashlist.append(hash)
             CACHE.append(identity)
 
@@ -64,7 +69,7 @@ def generate_hashlist(logs_dir: str):
 def check_and_load_cache():
     if os.path.exists(CACHE_FILE):
         print("[+] Found existing cache, loading...")
-        with open(CACHE_FILE, 'r') as file:
+        with open(CACHE_FILE, "r") as file:
             return [line.strip() for line in file.readlines()]
     else:
         return []
@@ -73,7 +78,7 @@ def check_and_load_cache():
 def save_cache():
     if not os.path.exists(CACHE_FILE):
         print("[-] No previous cache found, creating...")
-    with open(CACHE_FILE, 'w+') as file:
+    with open(CACHE_FILE, "w+") as file:
         file.write("\n".join(map(str, CACHE)))
 
 
@@ -87,22 +92,27 @@ def reset_cache():
 
 def main():
     # Parse Arguments
-    parser = argparse.ArgumentParser("credpoll",
+    parser = argparse.ArgumentParser(
+        "credpoll",
         description="Credential Polling for Responder",
     )
-    parser.add_argument('--responder-logs-dir',
-                        type=str,
-                        default="/usr/share/responder/logs",
-                        help="Responder logs directory to poll")
-    parser.add_argument('action',
-                        choices=['list', 'extract', 'hashlist', 'reset'],
-                        help='action to perform')
+    parser.add_argument(
+        "--responder-logs-dir",
+        type=str,
+        default="/usr/share/responder/logs",
+        help="Responder logs directory to poll",
+    )
+    parser.add_argument(
+        "action",
+        choices=["list", "extract", "hashlist", "reset"],
+        help="action to perform",
+    )
 
     args = parser.parse_args()
-    
+
     global CACHE
     CACHE = check_and_load_cache()
-    
+
     # Process directory and get absolute path
     if os.path.isabs(args.responder_logs_dir):
         logs_dir = args.responder_logs_dir
@@ -110,7 +120,7 @@ def main():
         logs_dir = os.path.abspath(args.responder_logs_dir)
 
     # Process Actions
-    if args.action == 'list':
+    if args.action == "list":
         new_accounts = list_accounts(logs_dir)
         if new_accounts:
             print("\n[+] The following accounts have newly captured hashes:\n")
@@ -119,24 +129,26 @@ def main():
         else:
             print("[-] No new hashes captured since the last rotation")
         if CACHE:
-            print("\n[+] The hashes for the following accounts were previously captured:\n")
+            print(
+                "\n[+] The hashes for the following accounts were previously captured:\n"
+            )
             for account in CACHE:
                 print(account)
-    elif args.action == 'extract':
+    elif args.action == "extract":
         hash = extract_creds(logs_dir)
-        print(f'\n{hash}')
-    elif args.action == 'hashlist':
+        print(f"\n{hash}")
+    elif args.action == "hashlist":
         hashlist = generate_hashlist(logs_dir)
         if hashlist:
             now = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
-            outfile_name = f'{os.getcwd()}/rotator-hashlist-{now}.hashes'
-            with open(outfile_name, 'w+') as file:
+            outfile_name = f"{os.getcwd()}/rotator-hashlist-{now}.hashes"
+            with open(outfile_name, "w+") as file:
                 file.write("\n".join(map(str, hashlist)))
             print(f"[+] Generated hashlist at {outfile_name}")
         else:
             print("[-] No new hashes captured since the last rotation")
         save_cache()
-    elif args.action == 'reset':
+    elif args.action == "reset":
         reset_cache()
 
     sys.exit(0)
--- credpoll/util.py
+++ credpoll/util.py
@@ -7,13 +7,13 @@ def get_cred_files(logs_dir: str) -> list:
     for file_name in os.listdir(logs_dir):
         # Exclude files ending in .log
         if file_name.split(".")[-1] != "log":
-            cred_files.append(f'{logs_dir}/{file_name}')
+            cred_files.append(f"{logs_dir}/{file_name}")
     return cred_files
 
 
 def get_hashes(cred_files: list) -> list:
     hashes = []
     for cred_file in cred_files:
-        with open(cred_file, 'r') as file:
+        with open(cred_file, "r") as file:
             hashes += [line.strip() for line in file.readlines()]
     return hashes
